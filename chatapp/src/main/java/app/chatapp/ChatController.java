947486acb57d956d11957d9a6d52a1c836f107b8
Hunter1202 <94958811+Hunter1202@users.noreply.github.com>
2025-01-18 17:59:02 +0700

    Add chat

---
+++ chatapp/src/main/java/app/chatapp/ChatController.java
@@ -0,0 +1,28 @@
+package app.chatapp;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @GetMapping("index")
+    public String index() {
+        return "index";
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(ChatMessage chatMessage) {
+        chatMessage.setType(ChatMessage.MessageType.JOIN);
+        return chatMessage;
+    }
+}
